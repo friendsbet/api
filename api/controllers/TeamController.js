c6b9f5d79e5becc9939beaaa35c0b5e3422b810d
Yann Bertrand <yann@bertrand.com>
2015-05-18 21:36:54 +0200

    feat(team): add the Team controller and model

---
+++ api/controllers/TeamController.js
@@ -0,0 +1,8 @@
+// TeamController.js
+//
+// @description :: Server-side logic for managing teams
+// @help        :: See http://links.sailsjs.org/docs/controllers
+
+module.exports = {
+	
+};
---
+++ api/models/Team.js
@@ -0,0 +1,48 @@
+// Team.js
+//
+// @description :: A Team object contains information about
+//                 a sport squad
+// @docs        :: http://sailsjs.org/#!documentation/models
+
+module.exports = {
+
+  schema: true,
+
+  attributes: {
+
+    // The complete name of the Team
+    // e.g 'England'
+    name: {
+      type: 'text',
+      notEmpty: true,
+      required: true,
+      unique: true
+    },
+
+    // A slug is a short name given to a Team
+    // This slug should be unique
+    // e.g 'eng'
+    slug: {
+      type: 'text',
+      alphanumericdashed: true,
+      notEmpty: true,
+      required: true,
+      unique: true
+    },
+
+    // Everything you need to know about this team
+    // e.g 'The England national rugby union team represents England in rugby union.'
+    description: {
+      type: 'text'
+    },
+
+    // The Games list associated with this Team
+    // References to Game objects
+    games: {
+      collection: 'game',
+      via: 'teams'
+    }
+
+  }
+};
+
