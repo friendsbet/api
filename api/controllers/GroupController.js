138df9eb914cc7ffdb36674065f6dfe931d34471
Yann Bertrand <yann@bertrand.com>
2015-05-18 21:43:08 +0200

    feat(group): add the Group controller and model

---
+++ api/controllers/GroupController.js
@@ -0,0 +1,8 @@
+// GroupController.js
+//
+// @description :: Server-side logic for managing groups
+// @help        :: See http://links.sailsjs.org/docs/controllers
+
+module.exports = {
+	
+};
---
+++ api/models/Group.js
@@ -0,0 +1,50 @@
+// Group.js
+//
+// @description :: A Group of Users
+// @docs        :: http://sailsjs.org/#!documentation/models
+
+module.exports = {
+
+  schema: true,
+
+  attributes: {
+
+    // The Group name
+    // e.g 'Friends Bet Group'
+    name: {
+      type: 'text',
+      notEmpty: true,
+      required: true,
+      unique: true,
+    },
+
+    // The cumulative score of this Group
+    // Updated at the end of a Game
+    // e.g 1024
+    score: {
+      type: 'integer',
+      defaultsTo: 0
+    }
+
+    // Everything you need to know about this group
+    // e.g 'The group of Friends Bet developers'
+    description: {
+      type: 'text'
+    },
+
+    // The admin of the Group
+    // It's a reference to a User
+    admin: {
+      model: 'user'
+    },
+
+    // The list of members
+    // References to User objects
+    members: {
+      collection: 'user',
+      via: 'groups'
+    }
+
+  }
+};
+
