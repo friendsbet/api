b8f2c633da97ae67db394bea581528f42ef9693e
Yann Bertrand <yann@bertrand.com>
2015-05-28 22:08:50 +0200

    feat(BetModel): check both teams score after validating the values

--- api/models/Bet.js
+++ api/models/Bet.js
@@ -4,6 +4,33 @@
 //                 It requires a score (a number) for each team.
 // @docs        :: http://sailsjs.org/#!documentation/models
 
+// Check if this Rugby score is possible
+//
+// See http://stackoverflow.com/questions/3464820/sum-of-numbers-making-a-sequence
+//  for more informations about the Frobenius coin problem
+//
+// @param score integer
+// @return boolean isScorePossible
+function checkRugbyScore(score) {
+  return score > 4 || score === 3;
+}
+
+// Check if this Football score is possible
+//
+// @param score integer
+// @return boolean isScorePossible
+function checkFootballScore(score) {
+  return score > -1;
+}
+
+// Check if this score is possible
+//
+// @param score integer
+// @return boolean isScorePossible
+function checkScore(score) {
+  return checkRugbyScore(score);
+}
+
 module.exports = {
 
   schema: true,
@@ -42,6 +69,21 @@ module.exports = {
       required: true
     }
 
+  },
+
+  // Check both Team scores after validating the values
+  afterValidate: function checkTeamScores(values, cb) {
+    var err = [];
+
+    _.forEach(['A', 'B'], function checkTeamScore(teamKey) {
+      if(!checkScore(values['scoreTeam' + teamKey]))
+        err.push({ msg: 'Team ' + teamKey + ' score (' + values['scoreTeam' + teamKey] + ') is not a valid rugby score.', });
+      
+      return;
+    });
+
+    return cb((err.length)? err: null);
   }
+
 };
 
