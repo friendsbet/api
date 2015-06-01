1b87d9929136cf030cc5b92a708fab38b089a8f2
Yann Bertrand <yann@bertrand.com>
2015-06-01 20:18:01 +0200

    feat(membership): create the controller and the model

---
+++ api/controllers/MembershipController.js
@@ -0,0 +1,8 @@
+// MembershipController.js
+//
+// @description :: Server-side logic for managing memberships
+// @help        :: See http://links.sailsjs.org/docs/controllers
+
+module.exports = {
+	
+};
---
+++ api/models/Membership.js
@@ -0,0 +1,13 @@
+// Membership.js
+//
+// @description :: A memberships makes the link between
+//                 a User and a Group
+// @docs        :: http://sailsjs.org/#!documentation/models
+
+module.exports = {
+
+  attributes: {
+
+  }
+};
+
