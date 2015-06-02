cf9ae193ef9c8dcd113085d0f7c950a5b0c5a4b2
Yann Bertrand <yann@bertrand.com>
2015-06-02 22:18:59 +0200

    tests(policies): add the sessionauth policy unit tests

---
+++ test/unit/policies/sessionAuth.test.js
@@ -0,0 +1,34 @@
+var should = require('should'),
+    sessionAuth = require('../../../api/policies/sessionAuth');
+
+describe('sessionAuthPolicy', function() {
+  this.slow(75);
+  this.timeout(2000);
+
+  it('should call forbidden function if the user is not authenticated', function (done) {
+
+    sessionAuth({
+      session: { authenticated: false }
+    }, {
+      forbidden: function (msg) {
+        should(msg).not.be.null;
+
+        msg.should.have.length;
+
+        return done();
+      }
+    });
+
+  });
+
+  it('should call next function if the user is authenticated', function (done) {
+
+    sessionAuth({
+      session: { authenticated: true }
+    }, null, function () {
+      return done();
+    });
+
+  });
+
+});
