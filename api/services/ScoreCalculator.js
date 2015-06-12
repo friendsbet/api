a1bf8f2d8d89681384a424c513663a5166d99d75
Yann Bertrand <yann.bertrand@orange.com>
2015-06-12 11:50:03 +0200

    feat(scorecalculator): implement the function to compute everything from a match id
    Kind of complicated... How could we improve that?

--- api/services/ScoreCalculator.js
+++ api/services/ScoreCalculator.js
@@ -2,6 +2,132 @@
 //
 // @description :: A service to manage scores of Bet, User and Group models
 
+function getMatch(matchId, cb) {
+  Match
+    .findOne(matchId)
+    .exec(cb);
+}
+
+function getUser(userId, cb) {
+  User
+    .findOne(userId)
+    .exec(cb);
+}
+
+
+function getGroups(groupsIds, cb) {
+  Group
+    .find(groupsIds)
+    .limit(0)
+    .exec(cb);
+}
+
+
+
+function getBetsFromMatch(matchId, cb) {
+  Bet
+    .find({ match: matchId })
+    .populate('user')
+    .limit(0)
+    .exec(function (err, bets) {
+      return cb(err, bets);
+  });
+}
+
+
+
+function getUserGroups(userId, cb) {
+  Membership
+    .find({ user: userId })
+    .populate('group')
+    .limit(0)
+    .exec(function (err, memberships) {
+      if(err || !memberships) {
+        return cb(err);
+      }
+      
+      getGroups(_.pluck(_.pluck(memberships, 'group'), 'id'), cb);
+  });
+}
+
+
+
+function updateBetScore(match, bet, cb) {
+  bet.score = match.importance; // ToDo
+
+  bet.save(cb);
+}
+
+
+
+function increaseUserScore(user, betScore, cb) {
+  user.score += betScore;
+
+  user.save(cb);
+}
+
+function increaseGroupsScore(group, betScore, cb) {
+  group.score += betScore;
+
+  group.save(cb);
+}
+
+
+
+function updateUserScoreFromBet(userId, betScore, cb) {
+  getUser(userId, function (err, user) {
+    if(err || !user) {
+      return cb(err);
+    }
+
+    increaseUserScore(user, betScore, cb);
+  });
+}
+
+
+
+function updateGroupsScores(groups, betScore, cb) {
+  async.each(groups, function (group, next) {
+    increaseGroupsScore(group, betScore, cb);
+  }, cb);
+}
+
+
+function updateGroupsScoresFromBet(userId, betScore, cb) {
+  getUserGroups(userId, function (err, groups) {
+    if(err) {
+      return cb(err);
+    }
+
+    updateGroupsScores(groups, betScore, cb);
+  })
+}
+
+
+
+function updateUsersAndGroupsScoresFromBet(bet, cb) {
+  async.parallel([
+    function (next) {
+      updateUserScoreFromBet(bet.user.id, bet.score, next);
+    },
+    function (next) {
+      updateGroupsScoresFromBet(bet.user.id, bet.score, next);
+    },
+  ], cb);
+}
+
+function updateScores(match, bets) {
+  async.each(bets, function (bet, next) {
+    updateBetScore(match, bet, function (err, newBet) {
+      if(err || !newBet) {
+        return next(err);
+      }
+
+      updateUsersAndGroupsScoresFromBet(newBet, next);
+    });
+  }, cb);
+}
+
 module.exports = {
 
   // computeAllScoresFromMatch
@@ -25,8 +151,22 @@ module.exports = {
     // 3. Update each of them
     // 4. Update users' scores
     // 5. Update users' groups' scores
-    async.series([
-
+    async.waterfall([
+      function (next) {
+        getMatch(matchId, function (err, match) {
+          return next(err, match);
+        });
+      },
+      function (match, next) {
+        getBetsFromMatch(matchId, function (err, bets) {
+          return next(err, match, bets);
+        });
+      },
+      function (match, bets, next) {
+        updateScores(match, bets, function (err) {
+          return next(err);
+        });
+      }
     ], cb);
   }
 
