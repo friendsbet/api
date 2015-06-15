70fd9c8a960852ae84ef9e93a0b2fac7a13087b3
Yann Bertrand <yann@bertrand.com>
2015-06-15 21:39:04 +0200

    tests(matchmodel): add the match model test file
    Also rename the group model test file

---
+++ test/unit/models/Matches.test.js
@@ -0,0 +1,28 @@
+var should = require('should');
+
+describe('MatchModel', function() {
+  this.slow(75);
+  this.timeout(2000);
+
+  var id = '';
+
+  describe('#find()', function() {
+
+    it('should return an empty array', function (done) {
+
+      Match
+        .find()
+        .exec(function(err, results) {
+          should(err).be.null;
+
+          should(results).not.be.undefined;
+          results.should.be.an.Array;
+          results.should.have.lengthOf(0);
+
+          done();
+      });
+
+    });
+
+  });
+});
