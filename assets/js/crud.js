e0e0cd2fd44392ee7b7706bbcd198832b373f9e6
Yann Bertrand <yann@bertrand.com>
2015-07-08 22:06:08 +0200

    feat(front): prepare the destroy feature

--- assets/js/crud.js
+++ assets/js/crud.js
@@ -1,7 +1,7 @@
 $(function() {
-  function update(event) {
-    var url = window.location.href;
+  var url = window.location.href;
 
+  function update(event) {
     // Do not send the form
     event.preventDefault();
 
@@ -45,5 +45,59 @@ $(function() {
     });
   }
 
+  function destroy() {
+    swal({
+      title: 'Are you sure?',
+      text: 'You will not be able to recover this instance!',
+      type: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#DD6B55',
+      confirmButtonText: 'Yes, delete it!',
+      closeOnConfirm: false,
+      closeOnCancel: false
+    }, function(isConfirm) {
+      if (isConfirm) {
+        var $form = $('form#update');
+        var id = $form.find('input#id')[0].value;
+        var data = {
+          _csrf: $form.find('input#_csrf')[0].value
+        };
+
+        // Prepare the request
+        var request = $.ajax({
+          method: 'DELETE',
+          url: url, // Current URL (findOne page)
+          data: data
+        });
+
+        request.done(function (message) {
+          var title = 'Deleted!';
+          var text = 'This instance has been deleted.\n' + 
+                      'You will be redirected as soon as you click "OK"';
+          var type = 'success';
+
+          swal({
+            title: title,
+            text: text,
+            type: type
+          }, function () {
+            window.location.href = url.substring(0, url.search(id) - 1);
+          });
+        });
+
+        request.fail(function (message) {
+          var title = 'Fail';
+          var text = 'The instance coulnd\'t be destroyed';
+          var type = 'error';
+          
+          swal(title, text, type);
+        });
+      } else {
+        swal('Cancelled', 'This instance is safe :)', 'error');
+      }
+    });
+  }
+
   $('form#update').on('submit', update);
+  $('form#update button#delete').on('click', destroy);
 });
