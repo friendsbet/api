8656fc3a695aecb4074cb435e6d9ee137e6a127d
Yann Bertrand <yann@bertrand.com>
2015-07-21 21:20:42 +0200

    feat(controllers): add bo actions to the team controller
    Warning: still broken

--- api/controllers/TeamController.js
+++ api/controllers/TeamController.js
@@ -4,5 +4,30 @@
 // @help        :: See http://links.sailsjs.org/docs/controllers
 
 module.exports = {
-	
+
+  boFind: function (req, res) {
+    Team
+      .find()
+      .exec(function (err, instances) {
+        if(err) return res.negotiate(err);
+
+        return res.ok(instances, 'teams/find.ejs');
+    });
+  },
+
+  boFindOne: function (req, res) {
+    Team
+      .findOne(req.param('id'))
+      .exec(function (err, instance) {
+        if(err) return res.negotiate(err);
+        if(!instance) return res.notFound(req.param('id'));
+
+        return res.ok(instance, 'teams/findOne');
+    });
+  },
+
+  boNew: function (req, res) {
+    return res.ok(null, 'teams/new');
+  }
+
 };
