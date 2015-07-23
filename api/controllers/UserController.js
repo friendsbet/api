8568d341b37bb8ee892bbce878380400e4ec7fd5
Yann Bertrand <yann@bertrand.com>
2015-07-23 21:49:18 +0200

    feat(controllers): add bo actions

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -5,4 +5,29 @@
 
 module.exports = {
 	
+  boFind: function (req, res) {
+    User
+      .find()
+      .exec(function (err, instances) {
+        if(err) return res.negotiate(err);
+
+        return res.ok({ users: instances }, 'users/find.ejs');
+    });
+  },
+
+  boFindOne: function (req, res) {
+    User
+      .findOne(req.param('id'))
+      .exec(function (err, instance) {
+        if(err) return res.negotiate(err);
+        if(!instance) return res.notFound(req.param('id'));
+
+        return res.ok({ user: instance }, 'users/findOne');
+    });
+  },
+
+  boNew: function (req, res) {
+    return res.ok(null, 'users/new');
+  }
+
 };
