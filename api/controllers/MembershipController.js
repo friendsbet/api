a9afe4d5c4c8c7116b47477a6a67b267ec0971a3
Yann Bertrand <yann@bertrand.com>
2015-07-25 00:22:07 +0200

    feat(controllers): create the bo actions

--- api/controllers/MembershipController.js
+++ api/controllers/MembershipController.js
@@ -4,5 +4,111 @@
 // @help        :: See http://links.sailsjs.org/docs/controllers
 
 module.exports = {
-	
+
+  boFind: function (req, res) {
+    Membership
+      .find()
+      .populate('user')
+      .populate('group')
+      .exec(function (err, instances) {
+        if(err) return res.negotiate(err);
+
+        return res.ok({ memberships: instances }, 'memberships/find');
+    });
+  },
+
+  boFindOne: function (req, res) {
+    var users = [];
+    var groups = [];
+    var membership = {};
+
+    async.parallel([
+
+      function getUsers(next) {
+        User
+          .find()
+          .limit(0)
+          .exec(function (err, instances) {
+            if(err) return next(err);
+
+            users = instances;
+
+            return next();
+        });
+      },
+
+      function getGroups(next) {
+        Group
+          .find()
+          .limit(0)
+          .exec(function (err, instances) {
+            if(err) return next(err);
+
+            groups = instances;
+
+            return next();
+        });
+      },
+
+      function getMembership(next) {
+        Membership
+          .findOne(req.param('id'))
+          .populate('user')
+          .populate('group')
+          .exec(function (err, instance) {
+            if(err) return next(err);
+
+            membership = instance;
+
+            return next();
+        });
+      }
+
+    ], function (err) {
+      if(err) return res.negotiate(err);
+      if(!membership) return res.notFound();
+
+      return res.ok({ users: users, groups: groups, membership: membership }, 'memberships/findOne');
+    });
+  },
+
+  boNew: function (req, res) {
+    var users = [];
+    var groups = [];
+
+    async.parallel([
+
+      function getUsers(next) {
+        User
+          .find()
+          .limit(0)
+          .exec(function (err, instances) {
+            if(err) return next(err);
+
+            users = instances;
+
+            return next();
+        });
+      },
+
+      function getGroups(next) {
+        Group
+          .find()
+          .limit(0)
+          .exec(function (err, instances) {
+            if(err) return next(err);
+
+            groups = instances;
+
+            return next();
+        });
+      }
+
+    ], function (err) {
+      if(err) return res.negotiate(err);
+
+      return res.ok({ users: users, groups: groups }, 'memberships/new');
+    });
+  },
+
 };
