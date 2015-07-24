1ae398d64a5c5c64b360936bd618bcf08b487868
Yann Bertrand <yann@bertrand.com>
2015-07-24 23:31:55 +0200

    feat(tasks): add some libraries in the pipeline

--- tasks/pipeline.js
+++ tasks/pipeline.js
@@ -26,6 +26,8 @@ var jsFilesToInject = [
   // Load sails.io before everything else
   'js/dependencies/sails.io.js',
   'js/dependencies/jquery.js',
+  'js/dependencies/bootstrap.js',
+  'js/dependencies/moment.js',
 
   // Dependencies like jQuery, or Angular are brought in here
   'js/dependencies/**/*.js',
