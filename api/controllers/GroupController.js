a5e5a14eef0041b745e983875074a1001e15d7db
Yann Bertrand <yann@bertrand.com>
2015-07-24 21:53:59 +0200

    feat(controllers): add the bo group actions

--- api/controllers/GroupController.js
+++ api/controllers/GroupController.js
@@ -5,4 +5,66 @@
 
 module.exports = {
 	
+  boFind: function (req, res) {
+    Group
+      .find()
+      .populate('technicalAdmin')
+      .exec(function (err, instances) {
+        if(err) return res.negotiate(err);
+
+        return res.ok({ groups: instances }, 'groups/find');
+    });
+  },
+
+  boFindOne: function (req, res) {
+    var group = {};
+    var users = [];
+
+    async.parallel([
+
+      function getGroup(next) {
+        Group
+          .findOne(req.param('id'))
+          .populate('technicalAdmin')
+          .exec(function (err, instance) {
+            if(err) return next(err);
+            
+            group = instance;
+
+            return next();
+        });
+      },
+
+      function getUsers(next) {
+        User
+          .find()
+          .limit(0)
+          .exec(function (err, instances) {
+            if(err) return next(err);
+
+            users = instances;
+
+            return next();
+        });
+      }
+
+    ], function (err) {
+      if(err) return res.negotiate(err);
+      if(!group) return res.notFound(req.param('id'));
+
+      return res.ok({ group: group, users: users }, 'groups/findOne');
+    });
+  },
+
+  boNew: function (req, res) {
+    User
+      .find()
+      .limit(0)
+      .exec(function (err, instances) {
+        if(err) return res.negotiate(err);
+
+        return res.ok({ users: instances }, 'groups/new');
+    });
+  }
+
 };
