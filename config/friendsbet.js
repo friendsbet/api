d555129cfb3d5023ff259e317c0e23cb09f7eee1
Yann Bertrand <yann@bertrand.com>
2015-07-27 22:07:31 +0200

    feat(config): move the sport config to a global friendsbet config file

---
+++ config/friendsbet.js
@@ -0,0 +1,16 @@
+// Friends Bet global settings
+//
+// Configuration of this instance of Friends Bet
+
+module.exports.FriendsBet = {
+
+  // What sport is currently in use?
+  currentSport: 'Rugby',
+
+  // Points given per bet
+  score: {
+    isGoodWinner: 100,
+    perTeamScoreDifference: [50, 25, 10]
+  }
+
+};
