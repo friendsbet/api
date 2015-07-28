0c7cfb017c38fd4b450ad41fc1b02dd8e6b655d0
Yann Bertrand <yann@bertrand.com>
2015-07-28 21:27:56 +0200

    test(scorecalculatorservice): add the computescoredifference tests

--- test/unit/services/ScoreCalculator.test.js
+++ test/unit/services/ScoreCalculator.test.js
@@ -135,19 +135,97 @@ describe('ScoreCalculator', function() {
       bet.scoreTeamA = 0;
       bet.scoreTeamB = 1;
 
-      ScoreCalculator.isGoodWinner(match, bet).should.be.true;
+      ScoreCalculator.betWinnerIsMatchWinner(match, bet).should.be.true;
 
       // Bet different score
       bet.scoreTeamA = 0;
       bet.scoreTeamB = 2;
 
-      ScoreCalculator.isGoodWinner(match, bet).should.be.true;
+      ScoreCalculator.betWinnerIsMatchWinner(match, bet).should.be.true;
 
       // Bet different score
       bet.scoreTeamA = 1;
       bet.scoreTeamB = 2;
 
-      ScoreCalculator.isGoodWinner(match, bet).should.be.true;
+      ScoreCalculator.betWinnerIsMatchWinner(match, bet).should.be.true;
+    });
+
+  });
+
+  describe('#computeScoreDifference()', function () {
+
+    it('should return 0 if betted team score and real team score are equal',
+        function () {
+      // Scores 0
+      var match = {
+        scoreTeamA: 0
+      };
+
+      var bet = {
+        scoreTeamA: 0
+      };
+
+      ScoreCalculator.computeScoreDifference('A', match, bet).should.equal(0);
+
+      // Scores 1
+      match.scoreTeamA = 1;
+      bet.scoreTeamA = 1;
+
+      ScoreCalculator.computeScoreDifference('A', match, bet).should.equal(0);
+    });
+
+    it('should return 1 if betted team score and real team score is different by 1',
+        function () {
+      var match = {
+        scoreTeamA: 0
+      };
+
+      var bet = {
+        scoreTeamA: 1
+      };
+
+      ScoreCalculator.computeScoreDifference('A', match, bet).should.equal(1);
+
+      match.scoreTeamA = 6;
+      bet.scoreTeamA = 5;
+
+      ScoreCalculator.computeScoreDifference('A', match, bet).should.equal(1);
+    });
+
+    it('should return 2 if betted team score and real team score is different by 2',
+        function () {
+      var match = {
+        scoreTeamA: 0
+      };
+
+      var bet = {
+        scoreTeamA: 2
+      };
+
+      ScoreCalculator.computeScoreDifference('A', match, bet).should.equal(2);
+
+      match.scoreTeamA = 5;
+      bet.scoreTeamA = 3;
+
+      ScoreCalculator.computeScoreDifference('A', match, bet).should.equal(2);
+    });
+
+    it('should return 3 if betted team score and real team score is different by 3',
+        function () {
+      var match = {
+        scoreTeamA: 0
+      };
+
+      var bet = {
+        scoreTeamA: 3
+      };
+
+      ScoreCalculator.computeScoreDifference('A', match, bet).should.equal(3);
+
+      match.scoreTeamA = 4;
+      bet.scoreTeamA = 1;
+
+      ScoreCalculator.computeScoreDifference('A', match, bet).should.equal(3);
     });
 
   });
