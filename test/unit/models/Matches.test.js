3df725f02d7230b530a89ba44f7fb07b9291782f
Yann Bertrand <yann@bertrand.com>
2015-12-15 23:02:15 +0100

    test(matches): add some matches model tests

--- test/unit/models/Matches.test.js
+++ test/unit/models/Matches.test.js
@@ -17,4 +17,48 @@ describe('MatchModel', function() {
     });
   });
 
+  describe('#name()', function () {
+
+    it('should convert team scores to integers', function (done) {
+      Match
+        .find()
+        .limit(1)
+        .populate('teamA')
+        .populate('teamB')
+        .exec(function (err, matches) {
+          should(err).be.null;
+          should(matches).not.be.undefined;
+
+          matches.should.be.an.array;
+
+          var match = matches[0];
+          match.should.be.an.object;
+          match.should.have.property('name');
+          match.name.should.be.a.function;
+          match.name().should.be.a.string;
+
+          // No need to check what's inside that string I hope :-)
+
+          return done();
+      });
+    });
+
+  });
+
+  describe('#beforeValidate()', function () {
+
+    it('should convert importance to float and teams scores to integer', function (done) {
+      Match.beforeValidate({
+          importance: '2.0',
+          scoreTeamA: '12',
+          scoreTeamB: '24'
+        }, function (err) {
+          should(err).be.undefined;
+
+          return done();
+      });
+    });
+
+  });
+
 });
