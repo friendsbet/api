e488f62c2584ca6ca6acadd6a10d18a348ecc8a2
Yann Bertrand <yann@bertrand.com>
2015-12-15 23:06:51 +0100

    test(users): add some users test

--- test/unit/models/Users.test.js
+++ test/unit/models/Users.test.js
@@ -17,4 +17,44 @@ describe('UserModel', function() {
     });
   });
 
+  describe('#name()', function () {
+
+    it('should return a string', function (done) {
+      User
+        .find()
+        .limit(1)
+        .exec(function (err, users) {
+          should(err).be.null;
+          should(users).not.be.undefined;
+
+          users.should.be.an.array;
+
+          var user = users[0];
+          user.should.be.an.object;
+          user.should.have.property('name');
+          user.name.should.be.a.function;
+          user.name().should.be.a.string;
+
+          // No need to check what's inside that string I hope :-)
+
+          return done();
+      });
+    });
+
+  });
+
+  describe('#beforeValidate()', function () {
+
+    it('should convert score to integer', function (done) {
+      User.beforeValidate({
+          score: '98',
+        }, function (err) {
+          should(err).be.undefined;
+
+          return done();
+      });
+    });
+
+  });
+
 });
