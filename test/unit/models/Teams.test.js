feda70732c3b6125fa0d62de72f2162845f99f1d
Yann Bertrand <yann@bertrand.com>
2015-12-16 22:59:14 +0100

    test(teams): check the destruction of matches linked to a team

--- test/unit/models/Teams.test.js
+++ test/unit/models/Teams.test.js
@@ -17,4 +17,97 @@ describe('TeamModel', function() {
     });
   });
 
+  describe('#destroy()', function () {
+
+    var teamAId;
+
+    before(function (done) {
+      async.auto({
+
+        createTeamA: function (next) {
+          Team.create({
+            name: 'Team A',
+            slug: 'team-a'
+          }).exec(next);
+        },
+
+        createTeamB: function (next) {
+          Team.create({
+            name: 'Team B',
+            slug: 'team-b'
+          }).exec(next);
+        },
+
+        createTeamC: function (next) {
+          Team.create({
+            name: 'Team C',
+            slug: 'team-c'
+          }).exec(next);
+        },
+
+        createFirstMatch: ['createTeamA', 'createTeamB', function (next, results) {
+          Match.create({
+            teamA: results['createTeamA'].id,
+            teamB: results['createTeamB'].id,
+            kickOffAt: new Date(),
+            stopBetsAt: new Date(),
+            venue: 'Nantes, France'
+          }).exec(next);
+        }],
+
+        createSecondMatch: ['createTeamA', 'createTeamC', function (next, results) {
+          Match.create({
+            teamA: results['createTeamC'].id,
+            teamB: results['createTeamA'].id,
+            kickOffAt: new Date(),
+            stopBetsAt: new Date(),
+            venue: 'Brest, France'
+          }).exec(next);
+        }]
+
+      }, function (err, results) {
+        should(err).be.null;
+
+        teamAId = results['createTeamA'].id;
+
+        return done();
+      });
+    });
+
+    it('should destroy linked matches', function (done) {
+
+      async.auto({
+
+        countMatchesBeforeDestruction: function (next) {
+          Match.count().exec(next);
+        },
+
+        destroyTeamA: ['countMatchesBeforeDestruction', function (next) {
+          Team.destroy(teamAId).exec(next);
+        }],
+
+        countMatchesAfterDestruction: ['destroyTeamA', function (next) {
+          Match.count().exec(next);
+        }]
+
+      }, function (err, results) {
+        should(err).be.null;
+        should(results).not.be.undefined;
+
+        results.should.be.an.array;
+        results['countMatchesBeforeDestruction'].should.be.a.Number;
+        results['countMatchesAfterDestruction'].should.be.a.Number;
+
+        numberOfMatchesBeforeDestruction = results['countMatchesBeforeDestruction'];
+        numberOfMatchesAfterDestruction = results['countMatchesAfterDestruction'];
+
+        (numberOfMatchesBeforeDestruction - numberOfMatchesAfterDestruction)
+          .should.equal(2);
+
+        return done();
+      });
+    });
+
+  });
+
 });
