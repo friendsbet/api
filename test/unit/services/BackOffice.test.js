d999024fd5fb7a3373da3e5fd33c7b44d38e4150
Yann Bertrand <yann@bertrand.com>
2015-12-22 22:57:59 +0100

    test(backoffice): add some instance getting tests

--- test/unit/services/BackOffice.test.js
+++ test/unit/services/BackOffice.test.js
@@ -2,47 +2,209 @@ var should = require('should');
 
 describe('BackOffice', function() {
 
-  describe('#getATeam(nonExistingId, cb)', function () {
-    it('should be alright if the id doesn\'t exist', function (done) {
-      BackOffice.getATeam('1', function (err, team) {
-        should(err).be.null;
-        should(team).be.undefined;
+  function checkResultIfUnnexistingId(method, cb) {
+    method('1', function (err, team) {
+      should(err).be.null;
+      should(team).be.undefined;
 
-        return done();
+      return cb();
+    });
+  }
+
+  function findAnInstanceWithExistingId(Model, field, fixtures, cb) {
+    var query = {};
+    query[field] = fixtures[0][field];
+
+    Model
+      .findOne(query)
+      .exec(function (err, instance) {
+        should(err).be.null;
+        should(instance).not.be.undefined;
+
+        instance.should.be.an.object;
+        instance.should.have.property('id');
+        instance.should.property('id').not.be.null;
+
+        return cb(instance.id);
+    });
+  }
+
+  function checkResultIfIdExists(method, instanceId, requiredProperty, cb) {
+    method(instanceId, function (err, instance) {
+      should(err).be.null;
+      should(instance).not.be.undefined;
+
+      instance.should.be.an.object;
+      instance.should.have.property(requiredProperty);
+
+      return cb();
+    });
+  }
+
+  describe('#getATeam', function () {
+    describe('(nonExistingId, cb)', function () {
+      it('should be alright if the id doesn\'t exist', function (done) {
+        checkResultIfUnnexistingId(BackOffice.getATeam, done);
+      });
+    });
+
+    describe('(id, cb)', function () {
+      var instanceId;
+
+      before(function (done) {
+        findAnInstanceWithExistingId(Team, 'name', fixtures['team'], function (_instanceId) {
+          instanceId = _instanceId;
+
+          return done();
+        });
+      });
+
+      it('should return an instance if it exists', function (done) {
+        checkResultIfIdExists(BackOffice.getATeam, instanceId, 'name', done);
       });
     });
   });
 
-  describe('#getATeam(id, cb)', function () {
-    var teamId;
-
-    before(function (done) {
-      Team
-        .findOne({
-          name: fixtures['team'][0].name
-        })
-        .exec(function (err, team) {
-          should(err).be.null;
-          should(team).not.be.undefined;
-
-          team.should.be.an.object;
-          team.should.have.property('id');
-          team.should.property('id').not.be.null;
-          teamId = team.id;
-
-          return done();
+  describe('#getAMatch', function () {
+    describe('(nonExistingId, cb)', function () {
+      it('should be alright if the id doesn\'t exist', function (done) {
+        checkResultIfUnnexistingId(BackOffice.getAMatch, done);
       });
     });
 
-    it('should return an instance if it exists', function (done) {
-      BackOffice.getATeam(teamId, function (err, team) {
-        should(err).be.null;
-        should(team).not.be.undefined;
+    describe('(id, cb)', function () {
+      var instanceId;
 
-        team.should.be.an.object;
-        team.should.have.property('name');
+      before(function (done) {
+        findAnInstanceWithExistingId(Match, 'kickOffAt', fixtures['match'], function (_instanceId) {
+          instanceId = _instanceId;
 
-        return done();
+          return done();
+        });
+      });
+
+      it('should return an instance if it exists', function (done) {
+        checkResultIfIdExists(BackOffice.getAMatch, instanceId, 'teamA', done);
+      });
+    });
+  });
+
+  describe('#getABet', function () {
+    describe('(nonExistingId, cb)', function () {
+      it('should be alright if the id doesn\'t exist', function (done) {
+        checkResultIfUnnexistingId(BackOffice.getABet, done);
+      });
+    });
+
+    describe('(id, cb)', function () {
+      var instanceId;
+
+      before(function (done) {
+        findAnInstanceWithExistingId(Bet, 'scoreTeamA', fixtures['bet'], function (_instanceId) {
+          instanceId = _instanceId;
+
+          return done();
+        });
+      });
+
+      it('should return an instance if it exists', function (done) {
+        checkResultIfIdExists(BackOffice.getABet, instanceId, 'scoreTeamA', done);
+      });
+    });
+  });
+
+  describe('#getAUser', function () {
+    describe('(nonExistingId, cb)', function () {
+      it('should be alright if the id doesn\'t exist', function (done) {
+        checkResultIfUnnexistingId(BackOffice.getAUser, done);
+      });
+    });
+
+    describe('(id, cb)', function () {
+      var instanceId;
+
+      before(function (done) {
+        findAnInstanceWithExistingId(User, 'email', fixtures['user'], function (_instanceId) {
+          instanceId = _instanceId;
+
+          return done();
+        });
+      });
+
+      it('should return an instance if it exists', function (done) {
+        checkResultIfIdExists(BackOffice.getAUser, instanceId, 'email', done);
+      });
+    });
+  });
+
+  describe('#getANotification', function () {
+    describe('(nonExistingId, cb)', function () {
+      it('should be alright if the id doesn\'t exist', function (done) {
+        checkResultIfUnnexistingId(BackOffice.getANotification, done);
+      });
+    });
+
+    describe('(id, cb)', function () {
+      var instanceId;
+
+      before(function (done) {
+        findAnInstanceWithExistingId(Notification, 'description', fixtures['notification'], function (_instanceId) {
+          instanceId = _instanceId;
+
+          return done();
+        });
+      });
+
+      it('should return an instance if it exists', function (done) {
+        checkResultIfIdExists(BackOffice.getANotification, instanceId, 'description', done);
+      });
+    });
+  });
+
+  describe('#getAMembership', function () {
+    describe('(nonExistingId, cb)', function () {
+      it('should be alright if the id doesn\'t exist', function (done) {
+        checkResultIfUnnexistingId(BackOffice.getAMembership, done);
+      });
+    });
+
+    describe('(id, cb)', function () {
+      var instanceId;
+
+      before(function (done) {
+        findAnInstanceWithExistingId(Membership, 'isAdmin', fixtures['membership'], function (_instanceId) {
+          instanceId = _instanceId;
+
+          return done();
+        });
+      });
+
+      it('should return an instance if it exists', function (done) {
+        checkResultIfIdExists(BackOffice.getAMembership, instanceId, 'isAdmin', done);
+      });
+    });
+  });
+
+  describe('#getAGroup', function () {
+    describe('(nonExistingId, cb)', function () {
+      it('should be alright if the id doesn\'t exist', function (done) {
+        checkResultIfUnnexistingId(BackOffice.getAGroup, done);
+      });
+    });
+
+    describe('(id, cb)', function () {
+      var instanceId;
+
+      before(function (done) {
+        findAnInstanceWithExistingId(Group, 'name', fixtures['group'], function (_instanceId) {
+          instanceId = _instanceId;
+
+          return done();
+        });
+      });
+
+      it('should return an instance if it exists', function (done) {
+        checkResultIfIdExists(BackOffice.getAGroup, instanceId, 'name', done);
       });
     });
   });
